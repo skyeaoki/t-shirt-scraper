f0027aae37e1b440b112f8b3a7b80941d656593b
Skye Aoki <skyeaoki@live.com>
2018-12-18 21:11:09 -0600

    updated to accommodate t-shirt website changing and added error handling for busy resource

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ fs.access('./data/', fs.constants.F_OK, () => {
 // Scrapes the URLs for the detail page of each tshirt
 const scrapeUrls =
   scrapeIt('http://shirts4mike.com/shirts.php', {
-		shirts: {
+    shirts: {
 			listItem: '.products li',
 			data: {
 				url: {
@@ -23,26 +23,42 @@ const scrapeUrls =
 					attr: 'href'
 				}
 			}
-		}
+    }
   });
+
+
+// Scrapes info from each T-shirt page
+const scrapeInfo = urlData => {
+  // Assign data to a global variable
+  urlObject = urlData;
+	// For each url from the first scrape, scrape the title, price, and image URL
+	for(let i = 0; i < urlObject.data.shirts.length; i++) {
+		scrapeIt(`http://shirts4mike.com/${urlObject.data.shirts[i].url}`, {
+      Title: 'title',
+			Price: '.price',
+			ImageUrl: {
+				selector: 'img',
+				attr: 'src'
+			}
+    // then push the scraped object to a global array
+  }).then( results => {
+        tshirtArray.push(results.data);
+        // then assign the corresponding url and time of scrape
+        assignUrlAndTime();
+        // then add 1 to the counter
+        count++
+        // If it's the last loop then execute the csv function
+        if(count === urlObject.data.shirts.length) {
+          csv();
+        }
+    })
+  }
+};
 
 // Assigns the corresponding url and the current time to the object
 const assignUrlAndTime = () => {
-  tshirtArray[count].Url= urlObject.shirts[count].url;
+  tshirtArray[count].Url= urlObject.data.shirts[count].url;
   tshirtArray[count].Time = JSON.stringify(new Date()).slice(12,20);
-};
-
-// Handles errors if they exist
-const errorHandler = error => {
-	if(error) {
-    // If an address cannot be found, log 404 error message to console
-    if(error.code == 'ENOENT') {
-      console.log(`404 error: Cannot connect to ${error.hostname}`);
-    // Otherwise log the error message to the console
-    } else {
-		  console.log('There was an error: ', error.message);
-    }
-  }
 };
 
 // Converts scrape results to csv format
@@ -51,31 +67,22 @@ const csv = () => {
   fs.writeFile(`data/${date}.csv`, result, errorHandler);
 };
 
-// Scrapes info from each T-shirt page
-const scrapeInfo = urlData => {
-  // Assign data to a global variable
-  urlObject = urlData;
-	// For each url from the first scrape, scrape the title, price, and image URL
-	for(let i = 0; i < urlObject.shirts.length; i++) {
-		scrapeIt(`http://shirts4mike.com/${urlObject.shirts[i].url}`, {
-      Title: 'title',
-			Price: '.price',
-			ImageUrl: {
-				selector: 'img',
-				attr: 'src'
-			}
-    // then push the scraped object to a global array
-    }).then( data => {
-        tshirtArray.push(data);
-        // then assign the corresponding url and time of scrape
-        assignUrlAndTime();
-        // then add 1 to the counter
-        count++
-        // If it's the last loop then execute the csv function
-        if(count === urlObject.shirts.length) {
-          csv();
-        }
-    })
+
+// Handles errors if they exist
+const errorHandler = error => {
+	if(error) {
+    // Connection error
+    if(error.code == 'ENOENT') {
+      console.error(`404 error: Cannot connect to ${error.hostname}`);
+    // File busy error
+    } else if(error.code == 'EBUSY') {
+      console.error("Resource is busy. Please close spreadsheet and run again to update today's scrape.");
+    // Otherwise log the error message to the console
+    } else {
+  		console.error('There was an error: ', error);
+    }
+  } else {
+    console.log("CSV created or updated");
   }
 };
 
